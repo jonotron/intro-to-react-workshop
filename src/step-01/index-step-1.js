b72656d2303047bf9d322534aca02d06f9255b1f
Jonathan Bowers <jonotron@gmail.com>
2019-05-14 13:20:30 -0700

    Convert step 1 to a water tracker

--- src/step-01/index-step-1.js
+++ src/step-01/index-step-1.js
@@ -8,9 +8,10 @@ import ReactDOM from 'react-dom'
 // just so our app doesn't look terrible
 import 'milligram'
 
-// this is a simple javascript object, we'll store the current scores in it
-const scores = {
-  jonathan: 4
+const name = 'Jonathan'
+// this is a simple javascript object, we'll store our states in it
+const stats = {
+  water: 2
 }
 
 // this is a React component, it's just a function that returns a DOM element
@@ -23,31 +24,10 @@ function App() {
   return (
     <div className="App">
       {/* this is a comment in jsx - we rarely use them*/}
-      <h1>My Board Game Scorer</h1>
+      <h1>{name + `'s`} Water Tracker</h1>
 
-      <table>
-        <thead>
-          <tr>
-            <th />
-            <th>Jonathan</th>
-            <th>Chad</th>
-            <th>Kaileen</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Score</td>
-            {/*
-              This 👇 is how you use javascript expressions in JSX. You just surround them with {}
-              You can do any expression you can think of in javascript inside the {}. That's the power
-              of JSX. It's just HTML + Javascript
-            */}
-            <td>{scores.jonathan}</td>
-            <td>3</td>
-            <td>6</td>
-          </tr>
-        </tbody>
-      </table>
+      <p>2 glasses of water</p>
+      {/* Replace the hard coded 2 with an expression that will evaluate to the amount of water */}
     </div>
   )
 }
@@ -56,8 +36,5 @@ function App() {
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
 
-// TODO: Add properties and values to the above scores object for chad and kaileen and replace those
-//       hard coded scores with the scores from the scores object, just like {scores.jonathan}
-
-// NEXT: Once you've done that, you can move onto the next file, index-step-2.js
+// NEXT: Once you're, you can move onto the next file, index-step-2.js
 //       Before you do, make sure the browser in code sandbox has the "current module view" toggled (blue)
