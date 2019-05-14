7217f6b70b6ce8dbdbc84f1eb2a0aa63236c0685
Jonathan Bowers <jonotron@gmail.com>
2019-05-14 13:48:47 -0700

    update step 2 to use bad click handler

--- src/step-02/index-step-2.js
+++ src/step-02/index-step-2.js
@@ -1,65 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import 'milligram'
+import { stat } from 'fs'
 
-const scores = [
-  { jonathan: 4, chad: 100, kaileen: 6 },
-  { jonathan: 3, chad: 100, kaileen: 5 }
-]
+const name = 'Jonathan'
+let stats = {
+  water: 2
+}
+
+// Let's add some functionality!
+// We'll add a button that increments our water count
+// Try it out below... check out the console under the browser on the right
+// What is going on? Why does this not work?
+
+// this is a little function that increments our water count
+function incWater() {
+  stats.water = stats.water + 1
+  // console.log displays stuff in the console tab (handy for debugging)
+  console.log(stats)
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>My Board Game Scorer</h1>
+      <h1>{name + `'s`} Water Tracker</h1>
 
-      <table>
-        <thead>
-          <tr>
-            <th />
-            <th>Jonathan</th>
-            <th>Chad</th>
-            <th>Kaileen</th>
-          </tr>
-        </thead>
-        <tbody>
-          {scores.map((score, i) => (
-            <tr key={i}>
-              <td>Round {i + 1}</td>
-              {/* TODO: Fix these td elements to display the rounds 
-                         score for each person */}
-              <td>{score.jonathan}</td>
-              <td>{score.chad}</td>
-              <td>{score.kaileen}</td>
-            </tr>
-          ))}
-        </tbody>
+      <button onClick={incWater}>+💧</button>
 
-        <tfoot>
-          <tr>
-            <th>Total</th>
-            <th>
-              <TotalScore player="jonathan" />
-            </th>
-            <th>{scores.reduce((acc, score) => acc + score.chad, 0)}</th>
-            <th>{scores.reduce((acc, score) => acc + score.kaileen, 0)}</th>
-          </tr>
-        </tfoot>
-      </table>
+      <p>{stats.water} 💧</p>
     </div>
   )
 }
 
-function TotalScore(props) {
-  return (
-    <span>{scores.reduce((acc, score) => acc + score[props.player], 0)}</span>
-  )
-}
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
 
-// TODO:
-// fix the elements with xxx to display the correct score
-// TODO:
-// make a new component the display the total score
-// for a player given all the scores and the player
+// React's paradigm, UI is a function of state
