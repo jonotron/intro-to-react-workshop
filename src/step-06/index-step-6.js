39702b285533fa7c4fa3d1e173c14b88669473b1
Jonathan Bowers <jonotron@gmail.com>
2019-05-14 17:27:12 -0700

    Loop over water records

--- src/step-06/index-step-6.js
+++ src/step-06/index-step-6.js
@@ -1,27 +1,73 @@
 import React from 'react'
+import { useState } from 'react'
 import ReactDOM from 'react-dom'
-
 import 'milligram'
-import ScoreBoard from './ScoreBoard'
 
-const scores = [
-  { jonathan: 4, chad: 3, kaileen: 6 },
-  { jonathan: 3, chad: 6, kaileen: 5 }
-]
+const name = 'Jonathan'
 
-/*
-Where did the TotalScore component go? It's in the TotalScore.js file... go have a peek
-
-TODO: Make a new file in the `step-6` directory called ScoreBoard, and see if you can
-create your own component that renders the <table> (and everything in it) from below.
-Also, move the scores into the new ScoreBoard component
-*/
 function App() {
+  const [stats, setStats] = useState([
+    { day: 3, water: 0 },
+    { day: 2, water: 3 },
+    { day: 1, water: 7 }
+  ])
+  const [water, setWater] = useState(0)
+  const incWater = amount => () => setWater(water + amount)
+
+  const logWater = amount => {
+    const day = stats[0].day + 1
+    setStats([{ day: day, water: amount }, ...stats])
+    setWater(0)
+  }
+
+  const red = '#880000'
+  const green = '#008800'
+  const style = {
+    padding: '0 10px',
+    backgroundColor: water >= 8 ? green : red,
+    color: '#ffffff'
+  }
   return (
     <div className="App">
-      <h1>My Board Game Scorer</h1>
+      <h1>{name + `'s`} Water Tracker</h1>
 
-      <ScoreBoard scores={scores} />
+      <button className="button" onClick={incWater(1)}>
+        +💧
+      </button>
+      <button className="button button-outline" onClick={incWater(-1)}>
+        -💧
+      </button>
+      <button className="button" onClick={incWater(4)}>
+        +💧💧💧💧
+      </button>
+      <button className="button button-outline" onClick={incWater(-4)}>
+        -💧💧💧💧
+      </button>
+      <p>
+        <span style={style}>{water}</span> 💧
+      </p>
+
+      <button className="button" onClick={() => logWater(water)}>
+        Log Day
+      </button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Index</th>
+            <th>Water</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {stats.map(record => (
+            <tr key={record.day}>
+              <td>{record.day}</td>
+              <td>{record.water}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
